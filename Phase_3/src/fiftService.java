ba7296563973ca4f89826760436cdcef410c1fc8
gytjd <hys339631@gmail.com>
2023-11-21 19:59:10 +0900

    [update] memberService 회원가입 execute 되지않는 오류 발생

--- Phase_3/src/fiftService.java
+++ Phase_3/src/fiftService.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
 
@@ -7,7 +8,7 @@ public class fiftService {
 
     static Scanner sc = new Scanner(System.in);
 
-    public static void start_Service(Connection conn, Statement stmt) {
+    public static void start_Service(Connection conn, Statement stmt) throws SQLException {
         System.out.println("Welcome to Our Shopping Mall");
 
         while(true) {
@@ -42,7 +43,7 @@ public class fiftService {
                 System.out.println("8) ");
                 System.out.println("9) ");
                 System.out.println("10) ");
-                System.out.println("Any Num) ");
+                System.out.println("Any Num) QUIT");
                 System.out.println("---------------------------");
 
                 serviceCom=sc.nextInt();
