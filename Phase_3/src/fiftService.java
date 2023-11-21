099771e48dd528db12137821085f3ca5ee613165
gytjd <hys339631@gmail.com>
2023-11-21 17:12:20 +0900

    [update] fiftService 기능 흐름 부분 break 오류 수정

--- Phase_3/src/fiftService.java
+++ Phase_3/src/fiftService.java
@@ -11,7 +11,7 @@ public class fiftService {
         System.out.println("Welcome to Our Shopping Mall");
 
         while(true) {
-            System.out.println("--------------------------");
+            System.out.println("---------------------------");
             System.out.println("Are you an existing member?");
             System.out.println("1.Yes");
             System.out.println("2.No");
@@ -30,57 +30,63 @@ public class fiftService {
             int serviceCom;
             boolean check_Flag=false;
 
-            System.out.println("Try the following functions");
-            System.out.println("1) ");
-            System.out.println("2) ");
-            System.out.println("3) ");
-            System.out.println("4) ");
-            System.out.println("5) ");
-            System.out.println("6) ");
-            System.out.println("7) ");
-            System.out.println("8) ");
-            System.out.println("9) ");
-            System.out.println("10) ");
-            System.out.println("Any Num) ");
-            serviceCom=sc.nextInt();
+            while(true) {
+                System.out.println("Try the following functions");
+                System.out.println("1) ");
+                System.out.println("2) ");
+                System.out.println("3) ");
+                System.out.println("4) ");
+                System.out.println("5) ");
+                System.out.println("6) ");
+                System.out.println("7) ");
+                System.out.println("8) ");
+                System.out.println("9) ");
+                System.out.println("10) ");
+                System.out.println("Any Num) ");
+                System.out.println("---------------------------");
 
-            switch (serviceCom) {
-                case 1:
-                    System.out.println(11);
+                serviceCom=sc.nextInt();
+
+                switch (serviceCom) {
+                    case 1:
+                        System.out.println(11);
+                        break;
+                    case 2:
+                        System.out.println(22);
+                        break;
+                    case 3:
+                        System.out.println(33);
+                        break;
+                    case 4:
+                        System.out.println(44);
+                        break;
+                    case 5:
+                        System.out.println(55);
+                        break;
+                    case 6:
+                        System.out.println(66);
+                        break;
+                    case 7:
+                        System.out.println(77);
+                        break;
+                    case 8:
+                        System.out.println(88);
+                        break;
+                    case 9:
+                        System.out.println(99);
+                        break;
+                    case 10:
+                        System.out.println(1010);
+                        break;
+                    case default:
+                        System.out.println("Good bye");
+                        check_Flag=true;
+                        break;
+                }
+                if(check_Flag){
                     break;
-                case 2:
-                    System.out.println(22);
-                    break;
-                case 3:
-                    System.out.println(33);
-                    break;
-                case 4:
-                    System.out.println(44);
-                    break;
-                case 5:
-                    System.out.println(55);
-                    break;
-                case 6:
-                    System.out.println(66);
-                    break;
-                case 7:
-                    System.out.println(77);
-                    break;
-                case 8:
-                    System.out.println(88);
-                    break;
-                case 9:
-                    System.out.println(99);
-                    break;
-                case 10:
-                    System.out.println(1010);
-                    break;
-                case default:
-                    System.out.println("Good bye");
-                    check_Flag=true;
-                    break;
+                }
             }
-
             if(check_Flag){
                 break;
             }
