eb7e906783aee576582e69fb2af817122190af30
halo0x15 <87225227+halo0x15@users.noreply.github.com>
2023-12-05 18:04:58 +0900

    feat : scripts.js 추가

---
+++ Phase_4/server/src/main/resources/View/scripts.js
@@ -0,0 +1,16 @@
+// 페이지 열기 함수
+function openMypage() {
+    // mypage.html로 이동
+    location.href = "mypage.html";
+}
+function openSignupForm() {
+    // mypage.html로 이동
+    location.href = "register.html";
+}
+function openLoginForm() {
+    // mypage.html로 이동
+    location.href = "login.html";
+}
+function goToMain() {
+    location.href = "main.html";
+}
