519603f2f093372eed6512340b56b4069d1c1fe8
halo0x15 <87225227+halo0x15@users.noreply.github.com>
2023-12-05 23:51:06 +0900

    refact : 파일경로수정

--- Phase_4/server/src/main/resources/templates/scripts.js
+++ Phase_4/server/src/main/resources/templates/scripts.js
@@ -1,21 +1,21 @@
 // 페이지 열기 함수
 function openMypage() {
     // mypage.html로 이동
-    location.href = "mypage.html";
+    location.href = "../mypage/mypage.html";
 }
 function openSignupForm() {
     // mypage.html로 이동
-    location.href = "registerpage.html";
+    location.href = "../register/registerpage.html";
 }
 function openLoginForm() {
     // mypage.html로 이동
-    location.href = "loginpage.html";
+    location.href = "../login/loginpage.html";
 }
 function goToMain() {
-    location.href = "mainpage.html";
+    location.href = "../main/mainpage.html";
 }
 function openProduct() {
-    location.href = "productpage.html";
+    location.href = "../product/productpage.html";
 }
 function openSizeChange() {
     location.href = "sizepage.html";
