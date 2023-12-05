453527d79b926b4fbcdc47de476e819671573e8d
gytjd <hys339631@gmail.com>
2023-12-05 11:53:50 +0900

    Comment : ProductService 주석 추가

--- Phase_4/server/src/main/java/fift/server/service/product/ProductService.java
+++ Phase_4/server/src/main/java/fift/server/service/product/ProductService.java
@@ -15,21 +15,39 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    /**
+     * 상품 전체 조회
+     * @return
+     */
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
+    /**
+     * 상품 상세 조회
+     * @param product_Id
+     * @return
+     */
     public Product getProduct(Long product_Id) {
         return productRepository.findById(product_Id)
                 .orElseThrow(()->new RuntimeException("Product Not Found"));
     }
 
+    /**
+     * 상품 삭제
+     * @param product_Id
+     */
     public void deleteProduct(Long product_Id) {
         Product productToDelete = productRepository.findById(product_Id)
                 .orElseThrow(() -> new RuntimeException("Product Not Found"));
         productRepository.delete(productToDelete);
     }
 
+    /**
+     * 상품 검색
+     * @param keyword
+     * @return
+     */
     public List<Product> searchProduct(String keyword) {
         return productRepository.findByNameContaining(keyword);
     }
