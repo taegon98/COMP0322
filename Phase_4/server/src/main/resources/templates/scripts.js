c75a882da9cb6f158746dbf699eeb47f6d4d87e3
halo0x15 <87225227+halo0x15@users.noreply.github.com>
2023-12-05 22:50:54 +0900

    feat : 경로수정

---
+++ Phase_4/server/src/main/resources/templates/scripts.js
@@ -0,0 +1,25 @@
+// 페이지 열기 함수
+function openMypage() {
+    // mypage.html로 이동
+    location.href = "../mypage/mypage.html";
+}
+function openSignupForm() {
+    // mypage.html로 이동
+    location.href = "../register/registerpage.html";
+}
+function openLoginForm() {
+    // mypage.html로 이동
+    location.href = "../login/loginpage.html";
+}
+function goToMain() {
+    location.href = "../main/mainpage.html";
+}
+function openProduct() {
+    location.href = "../product/productpage.html";
+}
+function openSizeChange() {
+    location.href = "../mypage/sizepage.html";
+}
+function openPassword() {
+    location.href = "../mypage/passwordpage.html";
+}
