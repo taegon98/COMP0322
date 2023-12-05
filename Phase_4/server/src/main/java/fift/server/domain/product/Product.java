90f149c2e7dc7681fbd28811c6d2e1323e48326c
gytjd <hys339631@gmail.com>
2023-12-05 10:50:33 +0900

    Feat : Product 클래스 생성 완료

---
+++ Phase_4/server/src/main/java/fift/server/domain/product/Product.java
@@ -0,0 +1,29 @@
+package fift.server.domain.product;
+
+import fift.server.domain.orderdetail.OrderDetail;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@RequiredArgsConstructor
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long product_Id;
+
+    @OneToMany(mappedBy = "product",cascade = CascadeType.ALL)
+    private List<OrderDetail> orderDetailList=new ArrayList<>();
+
+    private String Name;
+    private String Description;
+    private Long Price;
+    private String Photo;
+}
