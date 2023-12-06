c2d3415c21d985a0170ea73f58eff3a019fd108a
taegon1998 <taegon1998@gmail.com>
2023-12-06 21:08:10 +0900

    fix: balance 동기화 구현

--- Phase_4/server/src/main/java/fift/server/controller/order/OrderController.java
+++ Phase_4/server/src/main/java/fift/server/controller/order/OrderController.java
@@ -1,6 +1,10 @@
 package fift.server.controller.order;
 
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.transaction.annotation.Isolation;
 import fift.server.domain.customer.Customer;
 import fift.server.service.customer.CustomerService;
@@ -19,11 +23,22 @@ public class OrderController {
 
     private final CustomerService customerService;
     private final OrderService orderService;
-
     @GetMapping("customer/{customerId}/cartList/order")
-    public String cart_Order(@PathVariable("customerId") String id) {
-        System.out.println(1111);
+    public String cart_Order(@PathVariable("customerId") String id, HttpServletRequest request, HttpServletResponse response) {
         Customer customer = customerService.getCustomer(id);
+
+        HttpSession session = request.getSession();
+        session.setAttribute("customer", customer);
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("JSESSIONID")) {
+                cookie.setValue(session.getId());
+                response.addCookie(cookie);
+                break;
+            }
+        }
+
         orderService.order_Cart(customer);
         return "redirect:/";
     }
